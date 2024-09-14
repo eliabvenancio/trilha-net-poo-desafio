332e0cebb5066dbe7cc020d8f9ee8c4bba140374
Eliab Coelho Venâncio <eliab@ufv.br>
2024-09-14 17:56:18 -0300

    Implenta o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456789", "Modelo Nokia", "12121212", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iPhone = new Iphone("987654321", "Modelo iPhone", "45454545", 128);
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
